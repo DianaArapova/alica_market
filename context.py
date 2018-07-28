4db81558ea26866d764f66ded88346b8178ca319
Starcev Michail <starcev.m@skbkontur.ru>
2018-07-28 15:37:18 +0500

    add build_url

--- context.py
+++ context.py
@@ -1,10 +1,9 @@
-
 class Context:
     def __init__(self):
-        self.size = 42
-        self.style = ""
-        self.price = (0,180000)
-        self.color = "red"
+        self.size = None
+        self.style = None
+        self.price = None
+        self.color = None
         self.step = 0
 
     def set_size(self, size):
@@ -16,8 +15,28 @@ class Context:
     def set_color(self, color):
         self.color = color
 
-    def set_price(self, lower_price):
-        self.price = (lower_price, self.price[1])
+    def set_price(self, price):
+        self.price = price
 
     def build_url(self):
-        return ""
+        url = "https://www.lamoda.ru/c/369/clothes-platiya/?"
+        res = []
+        if self.size is not None:
+            res.append(f"size_values={self.size}")
+        if self.style is not None:
+            res.append(f"property_style={self.style}")
+        if self.price is not  None:
+            res.append(f"price={self.price[0]}%2C{self.price[1]}")
+        if self.color is not None:
+            res.append(f"colors={self.color}")
+
+        url += "&".join(res)
+        return url
+
+if __name__ == "__main__":
+    context = Context()
+    context.set_color(3865)
+    context.set_size(42)
+    context.set_price((799,3112))
+    context.set_style(5924)
+    print(context.build_url())
